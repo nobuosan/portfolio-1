d2e935d91e128ff5ee8d340612dadd5e361ce0cd
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-04-28 05:17:17 +0900

    complete base

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def profile
+  end
+
+  def work
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # get "/"
   root to:"users#index"
+  get "/profile" ,to:"users#profile"
+  get "/work" ,to:"users#work"
 end
